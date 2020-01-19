d4fd58f63a6847b99f4f064e97174b98574304ce
Norbert <norbert.gajda@gmail.com>
2020-01-19 20:49:53 +0100

    bonds-ytm

--- Angular-bonds/bonds-calculations/src/app/app.module.ts
+++ Angular-bonds/bonds-calculations/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BondsYtmCalcComponent } from './bonds-ytm-calc/bonds-ytm-calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BondsYtmCalcComponent
   ],
   imports: [
     BrowserModule,
